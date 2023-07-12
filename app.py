a50b30028c30c21bbd08a888f6459aa7798f7c39
Nithish Raja <109280236+NithishRajaOfficial@users.noreply.github.com>
2023-07-13 01:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
-
-
-
 app = Flask(__name__)
-model = pickle.load(open("model.pkl", 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -39,6 +35,10 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# In[ ]:
+
 
 
 
